09d0e4aab7813a0d15e362510d48d5c913d8ab8b
Tom Neyland <tcneyland+github@gmail.com>
2013-12-27 13:13:29 -0500

    removes bad files

--- dist/angular-dcjs.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-angular.module('angulaDc', []).directive('dcChart', function () {
-  return {
-    restrict: 'EAC',
-    scope: {},
-    link: function (scope, iElement, iAttrs) {
-    }
-  };
-});
