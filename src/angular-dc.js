ab7284377be7ad6ca046655b88a0d776fc58ac0a
Pierre Tardy <pierre.tardy@intel.com>
2014-04-11 18:26:13 +0200

    dc-select another directive for select integration
    Signed-off-by: Pierre Tardy <pierre.tardy@intel.com>

--- src/angular-dc.js
+++ src/angular-dc.js
@@ -193,3 +193,51 @@ angular.module('angularDc', [])
 
     }
 ]);
+
+/* Another directive, responsible of integration angular's select directive with dc.
+
+    <dc-select dc-dimension="fooDimension", all-label="All foos"/>
+
+   This directive helps to filter by arbitrary dimensions without need for another graph.
+*/
+angular.module('angularDc')
+.directive('dcSelect', ['$timeout', function ($timeout) {
+    return {
+        restrict: 'E',
+        scope: {
+            dcDimension: "=",
+            allLabel: "@"
+        },
+        template: "<select class='form-control' ng-model='selectModel' " + "ng-options='d.key for d in selectOptions'>",
+        link: function (scope, iElement, iAttrs) {
+            scope.$watch('dcDimension', function (dimension) {
+                var allkeys, chart;
+                if (dimension != null) {
+                    // we make a fake chart so that the dimension is known by dc.filterAll()
+                    chart = dc.baseMixin({});
+                    chart.dimension(dimension);
+                    chart.group(dimension);
+                    chart._doRender = function () {};
+                    chart._doRedraw = function () {};
+                    scope.selectModel = {
+                        key: scope.allLabel
+                    };
+                    allkeys = dimension.group().orderNatural().all();
+                    scope.selectOptions = [scope.selectModel].concat(allkeys);
+                }
+            });
+            return scope.$watch('selectModel', function (sel) {
+                if (scope.dcDimension != null) {
+                    if (sel !== null && sel.key !== scope.allLabel) {
+                        scope.dcDimension.filter(function (d) {
+                            return d === sel.key;
+                        });
+                    } else {
+                        scope.dcDimension.filter(null);
+                    }
+                    dc.redrawAll();
+                }
+            });
+        }
+    };
+}]);
